7f8c9ae108da225b5b24b63c45a7c86fb7c5fdec
Matthew Upson <matthew.a.upson@gmail.com>
2020-07-27 17:27:16 -0400

    new: Add terraform manifest

---
+++ terraform/sagemaker_test.tf
@@ -0,0 +1,82 @@
+
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    aws      = "~> 2.56"
+    dns      = "~> 2.2"
+    template = "~> 2.1"
+  }
+
+  backend "local" {
+  }
+}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+#
+# SageMaker ECR repo 
+#
+
+resource "aws_ecr_repository" "sagemaker_test" {
+  name = "sagemaker-test"
+
+}
+
+#
+# SageMaker Execution Role
+#
+
+module "sagemaker_execution_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "~> 2.0"
+
+  create_role = true
+
+  role_name             = "sagemaker-execution-role"
+  trusted_role_services = ["sagemaker.amazonaws.com"]
+
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess",
+  ]
+
+  role_requires_mfa = false
+}
+
+# S3 Bucket for use as DVC remote
+#
+
+module "sagemaker_test_s3" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket_prefix = "sagemaker-test"
+
+  versioning = {
+    enabled = false
+  }
+
+}
+
+#
+# Outputs
+#
+
+output "sagameker_ecr_repo_name" {
+  value = aws_ecr_repository.sagemaker_test.repository_url
+}
+
+output "sagameker_test_s3_bucket_name" {
+  value = module.sagemaker_test_s3.this_s3_bucket_id
+}
+
+output "sagameker_role_arn" {
+  value = module.sagemaker_execution_assumable_role.this_iam_role_arn
+}
+
